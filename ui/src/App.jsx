df92ff28c87af531d5697d1560a4476b37d55590
Pouya <zarepouya7@gmail.com>
2023-10-19 17:50:06 +0300

    Add react route dom to project

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,7 +1,14 @@
-export default function App() {
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom' 
+import Home from './pages/Home'
+function App() {
   return (
-    <h1 className="text-3xl bg-red-400">
-      Hello world!
-    </h1>
+      <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home/>}></Route>
+          </Routes>
+      </BrowserRouter>
   )
 }
+
+export default App
