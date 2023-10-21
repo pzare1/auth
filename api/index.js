559bb494613f44dff1a13efefa68e4e436743fc1
Pouya <zarepouya7@gmail.com>
2023-10-22 00:59:10 +0300

    add mongodb to project

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config();
 const app = express();
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log("connected to db")
+})
+.catch((err) => {
+    console.log(err)
+})
 const PORT = 8080
 app.listen(PORT, () => {
     console.log(`the server is running on the port ${PORT}`)
