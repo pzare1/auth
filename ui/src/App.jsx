c754666267b69e72dff7d450f85a99895fea0f27
Pouya <zarepouya7@gmail.com>
2023-10-21 23:24:12 +0300

    add link to project

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -3,15 +3,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import Header from './components/Header'
+import About from './pages/About'
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header/>
           <Routes>
             <Route path='/' element={<Home/>}></Route>
             <Route path='/register' element={<Register/>}></Route>
             <Route path='/login' element={<Login/>}></Route>
+            <Route path='/about' element={<About/>}></Route>
           </Routes>
       </BrowserRouter>
      </>
---
+++ ui/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+function Header() {
+  return (
+    <div className='bg-slate-200'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+            <h1 className="font-bold">Auth</h1>
+            <ul className='flex gap-4'>
+                <Link to="/about">
+                <li>About</li>
+                </Link>
+                <Link to="/login">
+                <li>Login</li>
+                </Link>
+                <Link to="/register">
+                <li>Register</li>
+                </Link>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Header
